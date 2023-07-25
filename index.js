00d6ef882d8acacf9f1ee4079e93861a37f8af43
Mohammednihaljm <mohammednihaljm@gmail.com>
2023-07-25 23:29:02 +0400

    learjs

---
+++ index.js
@@ -0,0 +1,43 @@
+// let count=0;
+
+// function increment(){
+// count=count+1
+// value.innerHTML=count
+// }
+// let value=document.getElementById("demo")
+// ==========================================
+// function save(){
+//     let countStr=count + "-"
+//     display.textContent+=countStr
+//     value.textContent=0
+//     count=0
+
+// }
+// let display=document.getElementById("demo2")
+
+// ==========================================================
+// // let firstName="mohammed"
+// // let secondName="nihal"
+
+// // let fullName=firstName+","+secondName;
+// // console.log(fullName);
+
+// // let value=document.getElementById("demo3")
+
+// // let message="hi"
+// // let firstName="mohammednihal"
+
+// // value.innerHTML=message+firstName;
+// ===================================================================
+
+//'hi nihal,good morning !'
+
+let message="hi"
+let firstName="nihal"
+let day="good morning"
+
+
+function Display(){
+console.log(message +" "+ firstName+","+day+"!");
+}
+Display()
