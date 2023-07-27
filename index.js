097fb0144699c53c5de3d5fd124be648297f9790
Mohammednihaljm <mohammednihaljm@gmail.com>
2023-07-27 18:19:58 +0400

    addmuldivsub

--- index.js
+++ index.js
@@ -32,12 +32,67 @@
 
 //'hi nihal,good morning !'
 
-let message="hi"
-let firstName="nihal"
-let day="good morning"
+// let message="hi"
+// let firstName="nihal"
+// let day="good morning"
 
 
-function Display(){
-console.log(message +" "+ firstName+","+day+"!");
+// function Display(){
+// console.log(message +" "+ firstName+","+day+"!");
+// }
+// Display()/
+// ------------------------------------------------
+
+// let myAmount=3
+
+// function addAmount3(){
+//     myAmount +=3
+// }
+
+// function minAmount1(){
+//     myAmount-=1
+// }
+
+// addAmount3()
+// addAmount3()
+// addAmount3()
+// minAmount1()
+// minAmount1()
+// console.log(myAmount);
+// -----------------------------------------------------
+
+// let buyItem=document.getElementById("buys")
+
+// function itemBuy(){
+//     buyItem.textContent="Somethhing went wrong please try again later"
+
+// }
+
+// -----------------------------------------------------------------
+let num1=8
+let num2=10
+
+let number1=document.getElementById("num1")
+let number2=document.getElementById("num2")
+let sum=document.getElementById("sum")
+
+function add(){
+    let result=num1+num2
+    sum.textContent="sum :"+result
+
 }
-Display()
+function mull(){
+    let result=num1*num2
+    sum.textContent="sum :"+result
+
+}
+function div(){
+    let result=num1/num2
+    sum.textContent="sum :"+result
+
+}
+function sub(){
+    let result=num1-num2
+    sum.textContent="sum :"+result
+
+}
