7598ba40eb761133352c73c6a966b5e6bda68695
Mohammednihaljm <mohammednihaljm@gmail.com>
2023-08-04 22:16:13 +0400

    buttons

--- index.js
+++ index.js
@@ -69,30 +69,59 @@
 // }
 
 // -----------------------------------------------------------------
-let num1=8
-let num2=10
+// let num1=8
+// let num2=10
 
-let number1=document.getElementById("num1")
-let number2=document.getElementById("num2")
-let sum=document.getElementById("sum")
+// let number1=document.getElementById("num1")
+// let number2=document.getElementById("num2")
+// let sum=document.getElementById("sum")
 
-function add(){
-    let result=num1+num2
-    sum.textContent="sum :"+result
+// function add(){
+//     let result=num1+num2
+//     sum.textContent="sum :"+result
 
-}
-function mull(){
-    let result=num1*num2
-    sum.textContent="sum :"+result
+// }
+// function mull(){
+//     let result=num1*num2
+//     sum.textContent="sum :"+result
 
-}
-function div(){
-    let result=num1/num2
-    sum.textContent="sum :"+result
+// }
+// function div(){
+//     let result=num1/num2
+//     sum.textContent="sum :"+result
 
-}
-function sub(){
-    let result=num1-num2
-    sum.textContent="sum :"+result
+// }
+// function sub(){
+//     let result=num1-num2
+//     sum.textContent="sum :"+result
 
-}
+// }
+// ----------------------------------------------------------------------------
+
+// let mark=80
+
+// if (mark>99){
+//     console.log("A");
+// }else if(mark>89){
+//     console.log("B");
+// }else if(mark>79){
+//     console.log("c");
+// }else if(mark>69)
+// {
+//     console.log("D");
+// }else{
+//     console.log("fail");
+// }
+// ----------------------------------------------------------------
+
+// let age=20
+
+// if(age < 18){
+//     console.log("not drink alchol");
+
+// }else if(age===18){
+//     console.log("drink");
+// }else{
+//     console.log("drink to alchol");
+// }
+// ------------------------------------------
